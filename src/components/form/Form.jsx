acb1bdf8cf51ff0d939d322444da5f96dd24108d
Ariel Corrado <jarielc2012@yahoo.com.ar>
2023-06-29 01:36:39 -0300

    cm

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import "./form.css";
+import { useContext } from 'react';
+import { IdiomaContext } from "../../context/IdiomaContext";
 
 const Form = () => {
+    const {isInglish} = useContext(IdiomaContext);
+
     return (
         <div className='contFormContacto flex column'>
             <form className='form OoS' action="/api/contacto.php" method="POST" acceptCharset="UTF-8">
-                <div className='contTituloContacto flex'><h2 className='titulosHome tituloHome-white titulo-contacto titulosFadeIn OoS'>ENVIANOS TU CONSULTA</h2></div>
-                <input type="text" name="name" className='formNom' placeholder='NOMBRE' />
-                <input type="text" name="" className='formTel' placeholder='APELLIDO' />
-                <input type="tel" name="" className='formEmail' placeholder='TELÉFONO' />
+                <div className='contTituloContacto flex'><h2 className='titulosHome tituloHome-white titulo-contacto titulosFadeIn OoS'>{(isInglish && "SEND US YOUR QUERY") || (!isInglish && "ENVIANOS TU CONSULTA")}</h2></div>
+                <input type="text" name="name" className='formNom' placeholder={(isInglish && "NAME") || (!isInglish && "NOMBRE")} />
+                <input type="text" name="" className='formTel' placeholder={(isInglish && "LAST NAME") || (!isInglish && "APELLIDO")} />
+                <input type="tel" name="" className='formEmail' placeholder={(isInglish && "PHONE") || (!isInglish && "TELÉFONO")} />
                 <input type="email" name="email" className='formEmpresa' placeholder='E-MAIL' />
-                <textarea type="text" name="msg" className='formMsj' placeholder='MENSAJE' rows="20" cols="45" />
-                <div className='contBotonContacto flex OoS'><button type="submit" className='botonForm'> Enviar </button></div>
+                <textarea type="text" name="msg" className='formMsj' placeholder={(isInglish && "MESSAGE") || (!isInglish && "MENSAJE")} rows="20" cols="45" />
+                <div className='contBotonContacto flex OoS'><button type="submit" className='botonForm'>{(isInglish && "SEND") || (!isInglish && "ENVIAR")}</button></div>
             </form>
         </div>
     );
--- src/components/reservasSeccion/Reservas.jsx
+++ src/components/reservasSeccion/Reservas.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import "./reservas.css";
 import Footer from '../footer/Footer';
 import Form from '../form/Form';
+import { useContext } from 'react';
+import { IdiomaContext } from "../../context/IdiomaContext";
 
 const ReservasSeccion = () => {
+    const {isInglish} = useContext(IdiomaContext);
       
     return (
         <>
             <div className='contSecciones seccionesIndividuales seccionReservas'>
-                <h2 className='titulosSecciones titulosReservas titulosFadeIn OoS'>Tarifas</h2>
-                <p className='subtitulosReservas OoS'>Consultá nuestras <br /> Tarifas y Promociones</p>
-                <button className='botonReservas OoS'>Ver Detalle</button>
+                <h2 className='titulosSecciones titulosReservas titulosFadeIn OoS'>{(isInglish && "RATES") || (!isInglish && "TARIFAS")}</h2>
+                <p className='subtitulosReservas OoS'>{(isInglish && "Check our") || (!isInglish && "Consultá nuestras")} <br /> {(isInglish && "Rates and promotions") || (!isInglish && "Tarifas y promociones")}</p>
+                <button className='botonReservas OoS'>{(isInglish && "See detail") || (!isInglish && "Ver Detalle")}</button>
                 <div className='rayita OoS'></div>
                 <Form/>
             </div>
